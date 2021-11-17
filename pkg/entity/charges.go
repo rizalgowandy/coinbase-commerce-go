acf2fd869db9c5a73ada209bf47c965f638e1dc1
ohmpatel <ohm.patel1997@gmail.com>
2021-11-18 02:08:43 +0530

    changed the metadata type

--- pkg/entity/charges.go
+++ pkg/entity/charges.go
@@ -9,18 +9,13 @@ import (
 // Reference: https://commerce.coinbase.com/docs/api/#create-a-charge
 
 type CreateChargeReq struct {
-	Name        string               `json:"name"`
-	Description string               `json:"description"`
-	LocalPrice  CreateChargePrice    `json:"local_price"`
-	PricingType enum.PricingType     `json:"pricing_type"`
-	Metadata    CreateChargeMetadata `json:"metadata"`
-	RedirectURL string               `json:"redirect_url"`
-	CancelURL   string               `json:"cancel_url"`
-}
-
-type CreateChargeMetadata struct {
-	CustomerID   string `json:"customer_id"`
-	CustomerName string `json:"customer_name"`
+	Name        string            `json:"name"`
+	Description string            `json:"description"`
+	LocalPrice  CreateChargePrice `json:"local_price"`
+	PricingType enum.PricingType  `json:"pricing_type"`
+	Metadata    map[string]string `json:"metadata"`
+	RedirectURL string            `json:"redirect_url"`
+	CancelURL   string            `json:"cancel_url"`
 }
 
 type CreateChargePrice struct {
